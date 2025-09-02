4175a78a6e1a2588e19e7ba5cfe7bee6908001cc
Booxxyy <andrent20@mail.ru>
2025-09-03 01:32:59 +0500

    Theme

---
+++ app/src/main/java/com/ig/igshop/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.ig.igshop.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+object JetShape{
+    val small = RoundedCornerShape(8.dp)
+    val medium = RoundedCornerShape(16.dp)
+    val large = RoundedCornerShape(16.dp)
+    val dialog = RoundedCornerShape(24.dp)
+}
